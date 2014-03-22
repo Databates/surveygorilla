b6cb4067b0adb037cb1addab3cc00c710aa16741
Ian <apprentice@dbc-chicago-021.local>
2014-03-22 16:48:57 -0500

    merging conflict

--- group4-gorilla/app/controllers/survey.rb
+++ group4-gorilla/app/controllers/survey.rb
@@ -133,8 +133,8 @@ end
 
   get '/survey/:survey_id' do
     @survey = Survey.find(params[:survey_id])
-
-    # erb :take_survey
+    @questions = @survey.questions # array
+    erb :"survey_views/take_survey"
   end
 
 

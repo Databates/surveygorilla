377771cc19123d9c09ed215e44b3f32d9c307dfb
Marco Morales <marco.bbcm@gmail.com>
2014-03-23 18:19:06 -0500

    Added results page data

--- group4-gorilla/app/controllers/index.rb
+++ group4-gorilla/app/controllers/index.rb
@@ -1,7 +1,11 @@
 get '/' do
-  # session.clear
-  @surveys = Survey.all
-  erb :index
+  @user = User.find(session[:user_id])
+  if session[:user_id]
+    redirect to("/user/#{@user.id}")
+  else
+    @surveys = Survey.all
+    erb :index
+  end
 end
 
 # not_found do
--- group4-gorilla/app/controllers/survey.rb
+++ group4-gorilla/app/controllers/survey.rb
@@ -1,44 +1,27 @@
-
-
-#---- Landing Page After Login/Signup:  Show ALL User Surveys ----------------
+#----Landing Page After Login/Signup:  Show ALL User Surveys ---------
 
 get '/surveys' do
 @surveys = Survey.all
-erb :"survey_views/display_survey"  #take them to display @ views/survey_views/display_survey.erb
+# take them to display @ views/survey_views/display_survey.erb
+erb :"survey_views/display_survey"
 end
 
-#------------------------------------------------------------------
-
-
-
-
-
-#--- Create a Survey Routes ------------------------------------------
+#---Create a Survey Routes--------------------------------------------
 
 #Unless someone wants the GET survey/new route, let's delete it:
 
 get '/survey/new' do
-  # if session[:user_id] == nil  #We can add in logic to check if current user
-  #   redirect '/'
-  # end
   erb :"survey_views/create_survey"
 end
 
-#FYI I removed duplicate (get '/survey/create')
-
-
 post '/survey/create' do
-
   survey = Survey.create(params[:survey])
-  session[:current_survey] = survey.id  #create survey session and set it equal to the new survey id
+  session[:current_survey] = survey.id
+  # create survey session and set it equal to the new survey id
   redirect '/survey/create/question'
 end
 
-#----------------------------------
-
-
-
-#------ Create a Question Route   -------------------
+#------Create a Question Route-------------------
 
 get '/survey/create/question' do
   # if session[:user_id] == nil  #Add this in to make sure the user is in a current session later.
@@ -84,80 +67,48 @@ post '/survey/create/answer_choices' do
   redirect '/surveys' #send them back to the landing page with ALL surveys
 end
 
-# Our jquery truck brings in answers like this:
-# {"answer1"=>"burrito", "answer2"=>"pizza", "answer3"=>"taco "}
+#----------Display an individual Survey to the User ------------------
 
-
-
-
-
-
-#----------Display an individual Survey to the User ----------
-get '/survey/:survey_id/individual' do #display_survey.erb sends users to display_one.erb,
-
+get '/survey/:survey_id/individual' do
   @survey = Survey.find_by_id(params[:survey_id])
   @questions = @survey.questions # array
-  # @questions.each {}
-  # @answer_choices =
-  # @question
-  # if @survey == nil
-  #   @error = true
-  # end
 
   erb :"survey_views/display_one"
 end
 
+#------------Take a Survey Route--------------------------------------
 
-
-#----------- Take a Survey Route ----------------------------------------
-
-#when you click "Take Survey" it uses this route:
 get '/survey/:survey_id' do
   @survey = Survey.find(params[:survey_id])
-  # @questions = @survey.questions # array
   erb :"survey_views/take_survey"
 end
 
-
 post '/survey/take' do
- # @survey = Survey.find(params[:survey_id])
- @survey = Survey.find_by_id(params[:survey_id])
+  @survey = Survey.find_by_id(params[:survey_id])
   @questions = @survey.questions
-  #@question = @survey.
 
   params['answers'].each do |key,value|
     Response.create(
       user: User.find(session[:user_id]),
       question: Question.find(key.to_i),
       answer_choice: AnswerChoice.find(value.to_i)
-    )
+    );
   end
   redirect to '/confirmation'
-
 end
 
-
-#redirect user to confirmation page after taking survey:
 get '/confirmation' do
   erb :"survey_views/completed_survey"
 end
 
-
-
-#------------ View a list of YOUR Surveys (not the global list on landing page)
+#--View a list of YOUR Surveys (not the global list on landing page)---
 
 get '/view_your_surveys' do
-  # if session[:user_id] == nil
-  #   redirect '/'
-  # end
-
   @surveys = User.find(session[:user_id]).surveys
   erb :"survey_views/list_user_surveys"
 end
 
-
-
-#----------- View the RESULTS of your Surveys ----------------
+#-----------View the RESULTS of your Surveys--------------------------
 
 get '/survey/:survey_id/results' do
   @survey = Survey.find_by_id(params[:survey_id])
@@ -168,34 +119,13 @@ get '/survey/:survey_id/results' do
   erb :"survey_views/results"
 end
 
-
-
-
-
-# TO DO: Delete functionality
-
-# ---------- Delete --------------------
+# ----------Delete----------------------------------------------------
 
 delete '/surveys/:id' do
   survey = Survey.find(params[:id])
   survey.delete
 end
 
-#----------------------------------------
-
-# --- We copied this from Nate's template. Not sure we need a Put/Update
-# ---------- Update (Created f)
-
-#
-# get '/surveys/edit/:id' do
-# # @surveys = Survey.find(params[:id])
-# # erb :"survey_views/edit_survey"
-# end
-
-# get '/surveys/:id' do
-#   @survey = Survey.find(params[:id])
-#   @survey.update(params)
-# erb :"survey_views/display_survey"
-# end
+#---------------------------------------------------------------------
 
 

e12f7fd7c82fb44c56350e7f0873d6a98cf6ec74
Greg Sucks <apprentice@dbc-chicago-031.local>
2014-03-22 19:00:15 -0500

    up-to-date

--- group4-gorilla/public/js/survey.js
+++ group4-gorilla/public/js/survey.js
@@ -1,6 +1,31 @@
 // Creating a Survey's Answer Options
 $(document).ready( function(){
 
+  $('#create-survey-form').on('submit', function(event) {
+    var surveyTitle = $('#create-survey-form input[name="survey[title]"]').val();
+    var surveyDescription = $('#create-survey-form input[name="survey[description]"]').val();
+    if (surveyTitle == false) {
+      $('#create-survey-validations').text("The Survey Title cannot be blank.").show().fadeOut( 2500 );
+    } else if (surveyDescription == false) {
+      $('#create-survey-validations').text("The Survey Description cannot be blank.").show().fadeOut( 2500 );
+    } else {
+      $('#create-question-form').show().fadeIn( 500 );
+      $('#create-survey-button, #create-survey-validations').fadeOut( 750 );
+    }
+    event.preventDefault();
+  });
+
+  $('#create-question-form').on('submit', function(event) {
+    var surveyQuestion = $('#create-question-form input[name="question[text]"]').val();
+    if (surveyQuestion == false) {
+      $('#create-question-validation').text("The question cannot be blank.").show().fadeOut( 2500 );
+    } else {
+      $('#create-question-form').show();
+    }
+    event.preventDefault();
+  });
+
+
   $('.add-option').on('click', function(event) {
     event.preventDefault();
     var i = $("#inputs .option-field").length + 1;

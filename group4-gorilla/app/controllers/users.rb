ecf2f1731f233a85900b71839d69dc75d73ee0c1
Marco Morales <marco.bbcm@gmail.com>
2014-03-23 19:17:47 -0500

    Cleaned up user routes

--- group4-gorilla/app/controllers/users.rb
+++ group4-gorilla/app/controllers/users.rb
@@ -1,47 +1,4 @@
-get '/user/signup' do
-  redirect to('/') if current_user?
-  erb :"user_views/new"
-end
-
-get '/user/signin' do
-  erb :"user_views/_signin_form"
-end
-
-get '/user/signout' do
-  session[:user_id] = nil
-  redirect to('/')
-end
-
-get '/user/delete' do
-  user = User.find(session[:user_id])
-  session.clear
-  user.destroy
-  redirect to('/')
-end
-
-#------ SIGN UP: New User ---------------------------------------
-
-post '/user/new' do
-  user = User.new(params[:user])
-  if user.save
-    session[:user_id] = user.id
-    redirect to("/user/#{user.id}") #get '/user/:id' route
-  else
-    @errors = user.errors.messages
-    erb :"user_views/new"
-  end
-end
-
-#---- user/id get route -----------------------------------------
-
-get '/user/:id' do
-  @user = User.find(session[:user_id])
-  @user_surveys = Survey.all.where(user_id: session[:user_id])
-  redirect to("/") if @user.id != params[:id].to_i
-  erb :"user_views/show"
-end
-
-#------ SIGN IN --> GO TO show.erb ------------------------------
+# SESSION routes -----------------------------------------------
 
 post '/user/signin' do
   user = User.find_by_email(params[:email])
@@ -55,14 +12,54 @@ post '/user/signin' do
   end
 end
 
-#----- GET Route to edit your account ---------------------------
+get '/user/signin' do
+  erb :"user_views/_signin_form"
+end
+
+get '/user/signout' do
+  session[:user_id] = nil
+  redirect to('/')
+end
+
+# CREATE routes -------------------------------------------------
+
+# SIGN UP: New User 
+get '/user/signup' do
+  redirect to('/') if current_user?
+  erb :"user_views/new"
+end
+
+post '/user/new' do
+  user = User.new(params[:user])
+  if user.save
+    session[:user_id] = user.id
+    redirect to("/user/#{user.id}") #get '/user/:id' route
+  else
+    @errors = user.errors.messages
+    erb :"user_views/new"
+  end
+end
+
+# READ routes ---------------------------------------------------
+
+# user homepage
+get '/user/:id' do
+  @user = User.find(session[:user_id])
+  @user_surveys = Survey.all.where(user_id: session[:user_id])
+  redirect to("/") if @user.id != params[:id].to_i
+  erb :"user_views/show"
+end
+
+# UPDATE routes --------------------------------------------------
+
+# GET Route to edit your account
 
 get '/user/edit/:id' do
   redirect to("/") if current_user.id != params[:id].to_i
   erb :"user_views/edit"
 end
 
-#----- EDIT YOUR ACCOUNT POST ROUTE (controllers/) -------------
+# POST Route to edit your account
 
 post '/user/edit' do
   @user = User.find(session[:user_id])
@@ -81,4 +78,12 @@ post '/user/edit' do
   end
 end
 
+# DELETE routes ------------------------------------------------------
+
+get '/user/delete' do
+  user = User.find(session[:user_id])
+  session.clear
+  user.destroy
+  redirect to('/')
+end
 

f9214f9e258286f6fec0227ca015495352076809
Ian <apprentice@dbc-chicago-021.local>
2014-03-23 22:55:15 -0500

    added validation on first answer choice. more to come tomorrow

--- group4-gorilla/public/js/survey.js
+++ group4-gorilla/public/js/survey.js
@@ -1,19 +1,71 @@
-// Creating a Survey's Answer Options
-$(document).ready( function(){
+// $(document).ready( function(){
 
-  $('.add-option').on('click', function(event) {
-    event.preventDefault();
-    var i = $("#inputs .option-field").length + 1;
-    var holder = $("<input class='option-field removable' type='text' name" + "='answer" + i +"'>");
-    holder.removeClass('template');
-    holder.addClass("removable");
-    holder.appendTo("#inputs");
-  });
+//   $('.add-option').on('click', function(event) {
+//     event.preventDefault();
+//     var i = $("#inputs .option-field").length + 1;
+//     var holder = $("<input class='option-field' type='text' name" + "='answer" + i +"'>");
+//     // holder.removeClass('template');
+//     // holder.addClass("removable");
+//     holder.appendTo("#inputs");
+//   });
 
-  $('.remove-last-option').on('click', function(event) {
-    event.preventDefault();
-    // $('.option-field.template').clone().show().toggleClass('template').appendTo('.option-field');
-    $(".removable:last").remove();
-    // console.log("hello");
-  });
-});
+//   $('.remove-last-option').on('click', function(event) {
+//     event.preventDefault();
+//     // $('.option-field.template').clone().show().toggleClass('template').appendTo('.option-field');
+//     $(".option-field:last").remove();
+//     // console.log("hello");
+//   });
+
+
+//   // Creating a Survey's Title and Description (with blank-field validations included)
+//   $('#submit-option-button').on("click", function(event) {
+
+//     // var answerChoice = $('#submit-option-form input[name="answer1"]').val();
+//     var answerChoice = $('#submit-option-form .option-field').val();
+
+//     // var otherChoices = $('#submit-option-form .removable').val();
+
+
+//     // var surveyDescription = $('#create-survey-form input[name="survey[description]"]').val();
+
+//     if (answerChoice == "") {
+//       event.preventDefault();
+//       $('#empty-warning').text("The Option field cannot be blank.").show().fadeOut( 2500 );
+//   }
+//     // } else if (otherChoices == "") {
+//     //  event.preventDefault();
+//     //   $('#empty-warning').text("The Survey Description cannot be blank.").show().fadeOut( 2500 );
+//     // }
+
+
+//     // var surveyTitle = $('#submit-option-form input[name="survey[title]"]').val();
+//   });
+// });
+
+
+
+
+
+
+
+
+
+// // Creating a Survey's Answer Options
+// $(document).ready( function(){
+
+//   $('.add-option').on('click', function(event) {
+//     event.preventDefault();
+//     var i = $("#inputs .option-field").length + 1;
+//     var holder = $("<input class='option-field removable' type='text' name" + "='answer" + i +"'>");
+//     holder.removeClass('template');
+//     holder.addClass("removable");
+//     holder.appendTo("#inputs");
+//   });
+
+//   $('.remove-last-option').on('click', function(event) {
+//     event.preventDefault();
+//     // $('.option-field.template').clone().show().toggleClass('template').appendTo('.option-field');
+//     $(".removable:last").remove();
+//     // console.log("hello");
+//   });
+// });

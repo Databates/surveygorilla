1da29de440636b721018dfb646a1a682d9a2c590
Marco Morales <marco.bbcm@gmail.com>
2014-06-09 10:21:23 -0700

    Adds guest user login form and form animations

--- public/js/login.js
+++ public/js/login.js
@@ -1,35 +1,119 @@
-// Client-side Email/Password Validations
 $(document).ready(function() {
 
+  // User forms animations
+
+  function activateForm(form) {
+    form.toggleClass('active');
+  }
+
+  function hideOtherForms(formToKeep) {
+    formToKeep.siblings().filter('.form')
+                         .removeClass('active')
+                         .animate({opacity: 0}, 'fast')
+                         .hide();
+  }
+
+  function closeForm(form) {
+    hideCloseButton();
+    form.slideUp('fast');
+    form.removeClass('active');
+  }
+
+  function showForm(form, callbk) {
+    form.slideDown('fast', callbk);
+  }
+
+  function showCloseButton() {
+    $('.button.close').css({display: 'block'}, 'fast');
+  }
+
+  function hideCloseButton() {
+    $('.button.close').fadeOut('fast');
+  }
+
+  function revealForm(form) {
+    form.animate({opacity: 1}, 'fast');
+  }
+
+  function concealForm(form, callbk) {
+    form.animate({opacity: 0}, 'fast', callbk);
+  }
+
+  function slideToNext(form) {
+    var currentHeight = parseInt(form.css('height'));
+    var nextHeight  = parseInt(currentHeight + 60);
+  
+    $('#forms').animate({height: nextHeight})
+               .css('overflow','visible');
+  }
+
+  function formViewController(form) {
+    var activeCount = $('#forms').find('.active').length;
+    var noneActive  = (activeCount === 0);
+    var isActive    = form.hasClass('active');
+
+    if ( noneActive ) {
+      showCloseButton();
+      showForm(form, function(){
+        activateForm(form);
+        revealForm(form);    
+      })
+    } else if ( isActive ) {
+      concealForm(form, function() {
+        $('#forms').css('height','auto');
+        hideCloseButton();
+        closeForm(form);
+      })
+    } else {
+      slideToNext(form);
+      hideOtherForms(form);
+      form.fadeIn( function(){
+        revealForm(form);
+        activateForm(form);
+      });
+    }
+  }
+
   $('#nav-signin').on('click', function(e) {
-      e.preventDefault();
-      $('#signup-box').hide();
-      $('#signin-box').slideToggle();
-    });
+    e.preventDefault();
+    formViewController($('#sign-in'));
+  })
 
-    $('#nav-signup').on('click', function(e) {
-      e.preventDefault();
-      $('#signin-box').hide();
-      $('#signup-box').slideToggle();
-    })
+  $('#nav-signup').on('click', function(e) {
+    e.preventDefault();
+    formViewController($('#sign-up'));
+  })
 
-  $('#sign-in').on("submit", function(event) {
+  $('#guest').on('click', function(e) {
+    e.preventDefault();
+    formViewController($('#guest-checkin'));
+  })
+
+  $('.button.close').on('click', function(e) {
+    e.preventDefault();
+    $('#forms').css('height','auto');
+    closeForm($('#forms .active'));
+  })
+
+  // Client-side Email/Password Validations
+
+  $('#sign-in').on("submit", function(e) {
     var sign_in_password = $('#sign-in input[name=password]').val();
     var sign_in_email = $('#sign-in input[name=email]').val();
 
     if ((sign_in_email == false) || (sign_in_password == false)) {
-      event.preventDefault();
-      alert("You're an idiot! You didn't complete the sign in form!");
+      e.preventDefault();
+      alert("Please fill in all the required fields.");
     }
   });
 
-  $('#sign-up').on("submit", function(event) {
-    var sign_up_email = $('#sign-up input[name="user[email]"]').val();
-    var sign_up_password = $('#sign-up input[name="user[password]"]').val();
-    var sign_up_password_confirmation = $('#sign-up input[name="user[password_confirmation]"]').val();
+  $('#sign-up').on("submit", function(e) {
+    var sign_up_email  = $('#sign-up input[name="user[email]"]').val();
+    var sign_up_pw     = $('#sign-up input[name="user[password]"]').val();
+    var sign_up_pwconf = $('#sign-up input[name="user[password_confirmation]"]').val();
 
-    if ((sign_up_email == false) || (sign_up_password == false) || (sign_up_password_confirmation == false)) {
-      event.preventDefault();
+    if ((sign_up_email == false) || (sign_up_pw == false) || (sign_up_pwconf == false)) {
+      e.preventDefault();
       alert("Please fill in all the required fields.");
     }
   });

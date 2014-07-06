344fbde9e67a3254d4331b82f7bc5dafdb6ffaa6
Marco Morales <marco.bbcm@gmail.com>
2014-07-06 15:28:45 -0700

    Adds top creators helper and view

--- app/helpers/survey.rb
+++ app/helpers/survey.rb
@@ -1,4 +1,9 @@
 helpers do
     include Rack::Utils
     alias_method :h, :escape_html
+
+    def top_creators
+      creator_counts = Survey.order("count_all desc").limit(10).group(:creator).count
+      # creator_counts.sort_by { |user, count| count }
+    end
 end

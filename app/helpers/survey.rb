ed10ed82a84ae79d4303ae6b5c653bfb84bdc1cf
Marco Morales <marco.bbcm@gmail.com>
2014-07-06 22:50:02 -0700

    Adds top creators method to completions, adds top creators view

--- app/helpers/survey.rb
+++ app/helpers/survey.rb
@@ -1,9 +1,8 @@
 helpers do
-    include Rack::Utils
-    alias_method :h, :escape_html
+  include Rack::Utils
+  alias_method :h, :escape_html
 
-    def top_creators
-      creator_counts = Survey.order("count_all desc").limit(10).group(:creator).count
-      # creator_counts.sort_by { |user, count| count }
-    end
+  def top_creators
+    Survey.order("count_all desc").limit(10).group(:creator).count
+  end
 end
